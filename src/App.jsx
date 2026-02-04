10a47ac772e562dafa945e74cd727bbf406d59c8
Arbaz sheikh <143587346+Arbaz0001@users.noreply.github.com>
2026-02-04 21:50:49 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import login from "./pages/login";
+import Login from "./pages/Login";
 import ProtectedRoute from "./routes/ProtectedRoute";
 import { AuthProvider } from "./context/AuthContext";
 
