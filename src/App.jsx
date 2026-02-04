7460730d3f1e583b44d60a7efb4f318897eb09e1
Arbaz sheikh <143587346+Arbaz0001@users.noreply.github.com>
2026-02-04 21:15:07 +0530

    Rename import from Login to login

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Login from "./pages/Login";
+import login from "./pages/login";
 import ProtectedRoute from "./routes/ProtectedRoute";
 import { AuthProvider } from "./context/AuthContext";
 
